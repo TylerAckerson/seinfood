c7120726d38615836ad2450931417003598d5849
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-10-26 07:24:44 -0700

    Cleans up the navbar

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -2,47 +2,47 @@ Navbar = React.createClass({
   handleLogOut: function(){
     ApiUtil.deleteSession();
   },
+
   render: function(){
-    var navbar;
+    var sign;
     var signOut =
       <input
        type="button"
        onClick={this.handleLogOut}
        value="Sign Out"
-       className="btn btn-defatul"/>;
+       className="sign-out"/>;
 
     if (window.CURRENT_USER_ID) {
-      navbar =
-        <ul className="nav navbar-nav navbar-right">
-          <li id="header-logo">
-            <a className="navbar-brand" href="/">
-              <img src="https://dl.dropboxusercontent.com/u/4448887/seinfood/Seinfeld.png"
-                   className="img" width="160" height="50"/>
-            </a>
-          </li>
-          <li><a href="/">Home</a></li>
-          <li><a href="#">Account</a></li>
-          <li>{signOut}</li>
-        </ul>;
+      sign =  <li>{signOut}</li>;
     } else {
-      navbar =
-        <ul className="nav navbar-nav navbar-right">
-          <li id="header-logo">
-            <a className="navbar-brand" href="/">
-              <img src="https://dl.dropboxusercontent.com/u/4448887/seinfood/Seinfeld.png"
-                   className="img" width="160" height="50"/>
-            </a>
-          </li>
-          <li><a href="/">Home</a></li>
-          <li><a href="users/new">Sign In</a></li>
-        </ul>;
+      sign =  <li><a href="users/new">Sign In</a></li>;
     }
 
     return (
       <nav className="navbar navbar-default" id="app-navbar">
-          <div className="navbar-header">
-            {navbar}
+      <div className="container-fluid">
+        <button type="button" className="navbar-toggle collapsed"
+                data-toggles="collapse" data-target="collapse-menu"
+                aria-expanded="false">
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+      </div>
+        <div className="navbar-header" id="header-logo">
+          <a className="navbar-brand" href="/">
+            <img src="https://dl.dropboxusercontent.com/u/4448887/seinfood/Seinfeld.png"
+                 className="img" width="160" height="50"/>
+          </a>
         </div>
+
+        <div className="collapse navbar-collapse" id="collapse-menu">
+          <ul className="nav navbar-nav pull-right">
+            <li><a href="/">Home</a></li>
+            <li>{sign}</li>
+          </ul>
+        </div>
+
       </nav>
     );
   }
