6e9495b82a4dda5228937eb0f4230f92c407b118
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-10-26 14:48:10 -0700

    Adds seed data for menus and cleans up sign in/ up page

--- app/assets/javascripts/components/restaurant_card_item.js.jsx
+++ app/assets/javascripts/components/restaurant_card_item.js.jsx
@@ -29,7 +29,7 @@ RestaurantCardItem = React.createClass({
 
     return (
       <div id="restaurant-card-item" onClick={this.props.onClick}
-                                  className="col-sm-2 col-md-2 col-lg-2 card text-center">
+                                  className="card text-center">
 
         <div className="card-item-header">
         </div>
--- app/assets/javascripts/components/restaurants_feature.js.jsx
+++ app/assets/javascripts/components/restaurants_feature.js.jsx
@@ -18,17 +18,15 @@ RestaurantsFeature = React.createClass({
     return (
       <div className="container-fluid">
         <div className="row text-center cards-container">
-          <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1"/>
-        {
-          this.props.cards.map(function(restaurant){
-            boundClick = this.handleDetailButton.bind(this, restaurant);
+            {
+              this.props.cards.map(function(restaurant){
+                boundClick = this.handleDetailButton.bind(this, restaurant);
 
-            return <RestaurantCardItem restaurant={restaurant}
-                                       onClick={boundClick}
-                                       key={restaurant.id}/>;
-          }.bind(this))
-      }
-        <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1"/>
+                return <RestaurantCardItem restaurant={restaurant}
+                                           onClick={boundClick}
+                                           key={restaurant.id}/>;
+              }.bind(this))
+          }
       </div>
     </div>
     );
