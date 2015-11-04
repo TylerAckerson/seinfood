e7f989383d7a95cf70bac3cbb740da3967a3eaf6
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-11-04 14:49:56 -0800

    Adds form elements to user component

--- app/assets/javascripts/components/user_account.js.jsx
+++ app/assets/javascripts/components/user_account.js.jsx
@@ -1,6 +1,9 @@
 UserAccount = React.createClass({
   getInitialState: function(){
-    return { user: UserStore.user() };
+    return { user: {  email: "",
+                    address: "",
+                       city: "",
+                      state: "" } };
   },
 
   componentDidMount: function() {
@@ -13,10 +16,37 @@ UserAccount = React.createClass({
   },
 
   render: function(){
-    debugger;
+    var user = this.state.user;
 
     return (
-      <div>user account</div>
+      <div className="container">
+        <h3>Account Info</h3>
+        <div className="form-group">
+          <label>Email
+            <input type="text" className="form-control" id="email"
+                   value={user.email}/>
+          </label>
+        </div>
+
+        <div className="form-group">
+          <label>Address
+            <input type="text" className="form-control" id="address"
+                   value={user.address}/>
+          </label>
+        </div>
+        <div className="form-group">
+          <label>City
+            <input type="text" className="form-control" id="city"
+                   value={user.city}/>
+          </label>
+        </div>
+        <div className="form-group">
+          <label>State
+            <input type="text" className="form-control" id="state"
+                   value={user.state}/>
+          </label>
+        </div>
+      </div>
     );
   }
 });
