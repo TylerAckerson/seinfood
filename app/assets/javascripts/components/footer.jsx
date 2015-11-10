3f6e958db0cd4b4d695f833c68e31b102b338b65
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-11-10 10:17:36 -0800

    Updates footer links to open in a new tab

--- app/assets/javascripts/components/footer.jsx
+++ app/assets/javascripts/components/footer.jsx
@@ -5,13 +5,13 @@ Footer = React.createClass({
 				<div className="container">
 					<div className="text-muted align-center">
 						<article>Creator:
-							<a href="http://tylerackerson.com"> Tyler Ackerson</a>
+							<a target="_blank" href="http://tylerackerson.com"> Tyler Ackerson</a>
 						</article>
 						<article>
 						Contact:
 						<a href="mailto:tyler.a.ackerson@gmail.com"> E-mail</a> |
-						<a href="https://github.com/tylerackerson"> Github</a> |
-						<a href="https://www.linkedin.com/in/tylerackerson"> Linkedin</a>
+						<a target="_blank" href="https://github.com/tylerackerson"> Github</a> |
+						<a target="_blank" href="https://www.linkedin.com/in/tylerackerson"> Linkedin</a>
 						</article>
 					<span>&copy; Seinfood 2015</span>
 					</div>
