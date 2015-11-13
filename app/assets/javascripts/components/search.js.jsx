b57e971a7d7c09c9227fa5cb04dea1c24940b195
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-11-12 16:38:51 -0800

    Adds styling/transition for search

--- app/assets/javascripts/components/search.js.jsx
+++ app/assets/javascripts/components/search.js.jsx
@@ -7,15 +7,15 @@ Search = React.createClass({
 
   handleFormSubmit: function(e){
     e.preventDefault();
+    $('.address-search').removeClass("hide col-md-1 col-lg-1");
+    $('.address-search').addClass("show col-md-9 col-lg-9");
     $('.search-button').removeClass("col-md-offset-4 col-lg-offset-4");
-    $('.search-button').addClass("col-md-offset-8 col-lg-offset-8");
-    $('.address-search').removeClass("hide");
 
     $(".address-search").typed({
         strings: ["129 West 81st St, New York, NY"],
         typeSpeed: 0
     });
-    
+
     setTimeout(function() {
       var searchString = this.state.search;
       this.props.history.pushState(null, "/restaurants", { search: searchString});
@@ -28,17 +28,17 @@ Search = React.createClass({
         <form role="form" className="form-inline search-form" onSubmit={this.handleFormSubmit}>
           <div className="container-fluid">
             <div className="row">
-              <h3>... from restaurants featured in the 90s sitcom Seinfeld</h3>
-            </div>
-            <div className="row">
               <h1>Order Food Delivery & Takeout</h1>
             </div>
+            <div className="row">
+              <h3>... from restaurants featured in the 90s sitcom Seinfeld</h3>
+            </div>
             <div className="row ">
-                  <input className="address-search col-xs-12 col-sm-12 col-md-8 col-lg-8 hide"
+                  <input className="address-search col-xs-12 col-sm-12 col-md-1 col-lg-1"
                          type="text"
                          valueLink={this.linkState("search")}>
                   </input>
-                  <input type="submit" className="search-button col-xs-6 col-sm-6 col-md-4 col-lg-4 col-md-offset-4 col-lg-offset-4" value="Show Restaurants"/>
+                  <input type="submit" className="search-button col-xs-6 col-sm-6 col-md-3 col-lg-3 col-md-offset-4 col-lg-offset-4" value="Find Restaurants"/>
            </div>
           </div>
         </form>
