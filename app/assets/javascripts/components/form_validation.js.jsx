2b03f53a12410f639dbace9f30019652a873385b
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-11-18 19:23:47 -0800

    Adds and updates state for the validation component

--- app/assets/javascripts/components/form_validation.js.jsx
+++ app/assets/javascripts/components/form_validation.js.jsx
@@ -1,4 +1,8 @@
 FormValidation = React.createClass({
+	getInitialState: function (){
+		return { checks: {"email": null, "address": null, "city": null, "state": null} };
+	},
+
 	componentWillReceiveProps: function(){
 			if (typeof this.props.type === "undefined") {
 				console.log("it's undefined");
@@ -6,18 +10,33 @@ FormValidation = React.createClass({
 				this.validateDeliveryFields();
 			}
 
-			this.validateFields();
+			this.validateEmail();
 	},
 
-	validateFields: function(){
-		console.log("validating fields" );
+	validateEmail: function(){
+		if (typeof this.props.user.email == "undefined") {
+			newChecks = $.extend(this.state.checks, { "email": "Email cannot be blank" });
+			this.setState( { checks: newChecks } );
+		}
 	},
 
 	validateDeliveryFields: function(){
-		console.log("checking extra fields" );
+		for (var check in this.state.checks) {
+			newChecks = $.extend({}, this.state.checks);
+			if (check != "email") {
+				console.log(this.props.user[check]); 
+				if (!this.props.user[check]){
+					newChecks[check] = check + " cannot be blank";
+				}
+			}
+		}
+
+		// console.log(newChecks);
+		this.setState( { checks: newChecks });
 	},
 
 	render: function(){
+		// console.log(this.state);
   	return(
 			<div>
 				<h4>Form being validated</h4>
